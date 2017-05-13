71bae9c455ca934388ef838fba715143f1a62a31
Nanami Tomozoe <nanami@tomozoe.com>
2017-05-13 11:14:48 +0900

    スクリーンショットの取得できた！

--- ScreenshotTest/ViewController.swift
+++ ScreenshotTest/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var screenShot: UIImage!
+    @IBOutlet var capturedImage : UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        super.viewDidLoad()
+      //  setup()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func setup() {
+         // キャプチャ画像を取得 capturedImageのimageにセット.
+        capturedImage.image = getScreenShot() as UIImage
+    }
+    
+    private func getScreenShot() -> UIImage {
+        // キャプチャする範囲を取得.
+        let rect = self.view.bounds
+        // ビットマップ画像のcontextを作成.
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        let context: CGContext = UIGraphicsGetCurrentContext()!
+        // 対象のview内の描画をcontextに複写する.
+        self.view.layer.render(in: context)
+        // 現在のcontextのビットマップをUIImageとして取得.
+        let capturedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        // contextを閉じる.
+        UIGraphicsEndImageContext()
+        
+        return capturedImage
+    }
+    
+    @IBAction func on (){
+        
+        setup()
+        
+    }
 
 }
 
